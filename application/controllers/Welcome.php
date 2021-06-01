4092ab12bba43c0cd3f2e9beb8367aabd45fe9e0
wahannuks <wkalingga@gmail.com>
2021-06-01 20:11:56 +0700

    add about us

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -20,6 +20,11 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$this->load->view('about');
+	}
+
+	public function about(){
+		$this->load->view('about');
 	}
 }
+
--- readme.rst
+++ readme.rst
@@ -1,5 +1,5 @@
 ###################
-What is CodeIgniter
+What is CodeIgniters
 ###################
 
 CodeIgniter is an Application Development Framework - a toolkit - for people
